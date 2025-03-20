c68fa95de644ed1f4489f904a001f7e7db0569b9
Anubhav Singh <anubhav.singh@kfintech.com>
2025-03-20 10:32:39 +0530

    final commit

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,17 +1,41 @@
+import React, { useState } from 'react';
+
 export function CreateTodo() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     return <div>
         <input style={{
             padding: '10px',
             margin: '10px',
-        }} type="text" placeholder="title"/> <br />
+        }} type="text" placeholder="title" onChange={(e) => {
+            const val = e.target.value;
+            setTitle(val);
+        }} /> <br />
         <input style={{
             padding: '10px',
             margin: '10px',
-        }} type="text" placeholder="description"/> <br />
+        }} type="text" placeholder="description" onChange={(e) => {
+            const val = e.target.value;
+            setDescription(val);
+        }} /> <br />
 
         <button style={{
             padding: '10px',
             margin: '10px',
+        }} onClick={() => {
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(async (res) => {
+                const json = await res.json();
+                alert("Todo added");
+            })
         }} >Add a todo</button>
     </div>
 }
